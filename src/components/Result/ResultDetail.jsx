0d19d5566f307776aae3e6a65c1390e8299c46e6
RushiChandalia <rushichandalia@gmail.com>
2021-06-07 01:45:55 +0530

    changes

--- src/components/Result/ResultDetail.jsx
+++ src/components/Result/ResultDetail.jsx
@@ -8,31 +8,29 @@ import LeftCountdown from "../Result/leftCountdown";
 import { GlobalContext } from "../../context/GlobalContext";
 import moment from "moment";
 
-
 const ResultDetail = () => {
-  
   const { id } = useParams();
   const { globalData } = useContext(GlobalContext);
   const history = useHistory();
 
-  if(globalData.selectedWallet === null){
-    console.log("empty")
-  history.push("/results")
+  if (globalData.selectedWallet === null) {
+    console.log("empty");
+    history.push("/results");
   }
-  
 
-  const { loading: ticketLoading, data: usertickets } = useQuery(
-    FETCH_USER_TICKET,
-    {
-      variables: {
-        walletID: Buffer.from(
-          globalData.selectedWallet.publicKey.toBytes()
-        ).toJSON().data,
-        LotteryId: parseInt(id),
-      },
-    }
-  );
-
+  const {
+    loading: ticketLoading,
+    data: usertickets,
+    refetch,
+  } = useQuery(FETCH_USER_TICKET, {
+    variables: {
+      walletID: Buffer.from(
+        globalData.selectedWallet.publicKey.toBytes()
+      ).toJSON().data,
+      LotteryId: parseInt(id),
+    },
+  });
+  useEffect(() => refetch(), []);
   const { loading: lotteryLoading, data: lottery } = useQuery(
     FETCH_LOTTERY_BY_ID,
     { variables: { Id: parseInt(id) } }
@@ -41,11 +39,11 @@ const ResultDetail = () => {
   if (lotteryLoading || ticketLoading) {
     return <Loader />;
   } else {
-    console.log(usertickets.getUserTickets)
+    console.log(usertickets.getUserTickets);
     return (
       <div className="detailSection">
         <div className="topSection">
-          <RDetail globalData = {globalData} data={lottery.getLotteryById} />
+          <RDetail globalData={globalData} data={lottery.getLotteryById} />
           <LeftCountdown
             lotteryData={lottery.getLotteryById}
             ticketData={usertickets.getUserTickets}
@@ -54,13 +52,23 @@ const ResultDetail = () => {
         <div className="bottomSectionResult gradientBg2">
           <div id="ticket-details">
             <h4>Solana Pick 6</h4>
-            <h4>{moment(lottery.getLotteryById.EndDate).format('LL')}</h4>
+            <h4>{moment(lottery.getLotteryById.EndDate).format("LL")}</h4>
             <br></br>
             <p>Your Number</p>
-            {usertickets.getUserTickets.map((t,i)=>{
+            {usertickets.getUserTickets.map((t, i) => {
               // console.log(t.ticketArray[0])
-              return <p key={i}>{t.ticketArray[0]}&nbsp;&nbsp;{t.ticketArray[1]}&nbsp;&nbsp;{t.ticketArray[2]}&nbsp;&nbsp;{t.ticketArray[3]}&nbsp;&nbsp;{t.ticketArray[4]}&nbsp;&nbsp;{t.ticketArray[5]}&nbsp;&nbsp;</p>
-              
+              var cha = globalData.charities.find((c)=>(c.ID ===t.charityId))
+              console.log(cha)
+              return (
+                <>
+                  <p key={i}>
+                    {t.ticketArray[0]}&nbsp;&nbsp;{t.ticketArray[1]}&nbsp;&nbsp;
+                    {t.ticketArray[2]}&nbsp;&nbsp;{t.ticketArray[3]}&nbsp;&nbsp;
+                    {t.ticketArray[4]}&nbsp;&nbsp;{t.ticketArray[5]}&nbsp;&nbsp;
+                  </p>{" "}<p>{cha.charityName}</p>
+                
+                </>
+              );
             })}
           </div>
         </div>
