fbf3bc2f0b54ada89cfd0517d83d47487d35ef87
RushiChandalia <rushichandalia@gmail.com>
2021-08-18 12:22:50 +0530

    changing in purchase ticket

--- src/renderer/components/purchase/utils/ticketPurchase.ts
+++ src/renderer/components/purchase/utils/ticketPurchase.ts
@@ -5,11 +5,12 @@ import {
   TransactionInstruction,
   Account,
   SYSVAR_RENT_PUBKEY,
-  LAMPORTS_PER_SOL,
+  sendAndConfirmRawTransaction,
 } from "@solana/web3.js";
 import store from "../../../redux/stores/store";
 import CryptoJS from "crypto-js";
 import { Buffer } from "buffer";
+
 /*eslint-disable  @typescript-eslint/no-non-null-assertion*/
 /*eslint-disable  @typescript-eslint/no-explicit-any */
 export const ticketPurchase = async (purchaseDataArr: {
@@ -19,13 +20,11 @@ export const ticketPurchase = async (purchaseDataArr: {
   lotteryDataAccount: string;
 }): Promise<any> => {
   const state = store.getState();
-
   const globalData = state.globalData;
 
   const charityPK = globalData.charities.charities.find(
     (c) => c.id === purchaseDataArr.charityId
   );
-
   const lotteryDataAccountBytes = CryptoJS.AES.decrypt(
     purchaseDataArr.lotteryDataAccount,
     process.env.REACT_APP_SECRET_KEY!
@@ -47,13 +46,13 @@ export const ticketPurchase = async (purchaseDataArr: {
   ) {
     try {
       const data = {
-        charity: charityPK?.publicKey,
-        user_wallet_pk: globalData.selectedWallet.publicKey.toBytes(),
+        charity: new PublicKey(charityPK?.publicKey as string),
+        user_wallet_pk: globalData.selectedWallet.publicKey,
         ticket_number_arr: purchaseDataArr.ticketNumArr,
       };
-      console.log(data);
-      const dataArr = new (Buffer as any).alloc(71, JSON.stringify(data)!);
-
+      console.log(data)
+      const dataArr = Buffer.from(JSON.stringify(data))
+      
       const ticketDataAccount = new Account();
       const createTicketDataAccountTx = SystemProgram.createAccount({
         space: 71,
@@ -64,6 +63,7 @@ export const ticketPurchase = async (purchaseDataArr: {
         fromPubkey: globalData.selectedWallet.publicKey,
         newAccountPubkey: ticketDataAccount.publicKey,
         programId: lotteryInitProgramId,
+      
       });
 
       const purchaseTicketTx = new TransactionInstruction({
@@ -71,7 +71,7 @@ export const ticketPurchase = async (purchaseDataArr: {
         keys: [
           {
             pubkey: lotteryDataAccount.publicKey,
-            isSigner: false,
+            isSigner: true,
             isWritable: true,
           },
           {
@@ -82,42 +82,66 @@ export const ticketPurchase = async (purchaseDataArr: {
           {
             pubkey: globalData.selectedWallet.publicKey,
             isSigner: true,
-            isWritable: false,
+            isWritable: true,
           },
           {
             pubkey: holdingWalletPK,
-            isSigner: true,
-            isWritable: false,
+            isSigner: false,
+            isWritable: true,
           },
           { pubkey: SYSVAR_RENT_PUBKEY, isSigner: false, isWritable: false },
-          { pubkey: holdingWalletPK, isSigner: false, isWritable: false },
+          { pubkey: lotteryInitProgramId, isSigner: false, isWritable: false },
         ],
         data: dataArr,
       });
+      console.log(typeof dataArr)
       const transaction = new Transaction().add(
         createTicketDataAccountTx,
         purchaseTicketTx
       );
-
-      const signers = [lotteryDataAccount];
+      console.log(purchaseTicketTx)
+      const signers = [ticketDataAccount,lotteryDataAccount];
+      transaction.feePayer = globalData.selectedWallet.publicKey;
+      transaction.setSigners(
+        globalData.selectedWallet.publicKey,
+        ...signers.map((s) => s.publicKey),
+      );
 
       transaction.recentBlockhash = (
         await globalData.connection.getRecentBlockhash()
       ).blockhash;
-      transaction.setSigners(
-        globalData.selectedWallet.publicKey,
-        ...signers.map((s) => s.publicKey)
-      );
-
+    
+      
+      transaction.partialSign(...signers);
+      
       const signedTx = await globalData.selectedWallet.signTransaction(
         transaction
       );
-      const signature = await globalData.connection.sendTransaction(
-        signedTx,
-        signers
-      );
-      await globalData.connection.confirmTransaction(signature, "singleGossip");
 
+      console.log(signedTx.signatures[0].publicKey.toBase58());
+      console.log(signedTx.signatures[1].publicKey.toBase58());
+      console.log(signedTx.signatures[2].publicKey.toBase58());
+      console.log(signedTx.signatures[0].signature);
+      console.log(signedTx.signatures[1].signature);
+      console.log(signedTx.signatures[2].signature);
+      console.log(signedTx.feePayer?.toBase58());
+      // console.log( signedTx.serialize() )
+      // const transactionB64 = Buffer.from(signedTx.serialize()).toString('base64')
+      // console.log(transactionB64)
+      // const signature = await globalData.connection.sendEncodedTransaction(
+      //   transactionB64,{preflightCommitment:"singleGossip"}
+      // );
+
+      // await globalData.connection.confirmTransaction(signature, "singleGossip");
+
+      // signedTx.sign(...[lotteryDataAccount])
+      // console.log(transaction.signatures[0].publicKey.toBase58())
+      // console.log(ticketDataAccount.publicKey.toBase58())
+
+      const signature = await sendAndConfirmRawTransaction(
+        globalData.connection,
+        signedTx.serialize(),
+      )
       return {
         DataWallet: ticketDataAccount.publicKey.toBytes(),
         success: true,
